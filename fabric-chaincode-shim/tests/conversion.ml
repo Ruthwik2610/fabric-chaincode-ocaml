a8e4709d083419f5d4f4452246938d6773884288
Ruthwik Krishna Bandreddy <86421510+Ruthwik2610@users.noreply.github.com>
2024-09-26 13:04:21 +0530

    Update conversion.ml
    updated to support fabric 2.5
    
    
    Signed-off-by: Ruthwik Krishna Bandreddy <86421510+Ruthwik2610@users.noreply.github.com>

--- fabric-chaincode-shim/tests/conversion.ml
+++ fabric-chaincode-shim/tests/conversion.ml
@@ -1,13 +1,19 @@
 let () =
+  (* Create a timestamp with 42 seconds *)
   let timestamp = FabricChaincodeShim.Protobuf.Timestamp.make ~seconds:42 () in
-  Printf.printf "%i\n" timestamp.seconds;
+  Printf.printf "Original seconds: %i\n" timestamp.seconds;
+
+  (* Encode the timestamp to protobuf format *)
   let encoded =
     FabricChaincodeShim.Protobuf.Timestamp.to_proto timestamp
     |> Ocaml_protoc_plugin.Writer.contents
   in
-  Printf.printf "%S\n" encoded;
+  Printf.printf "Encoded timestamp: %S\n" encoded;
+
+  (* Decode the protobuf back to a timestamp *)
   let decoded =
     Ocaml_protoc_plugin.Reader.create encoded
-    |> FabricChaincodeShim.Protobuf.Timestamp.from_proto |> Result.get_ok
+    |> FabricChaincodeShim.Protobuf.Timestamp.from_proto
+    |> Result.get_ok
   in
-  Printf.printf "%i\n" decoded.seconds
+  Printf.printf "Decoded seconds: %i\n" decoded.seconds
