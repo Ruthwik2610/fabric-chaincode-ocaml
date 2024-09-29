c69e4bfc88ef2b8e324a288e4ec6c1d803d6795e
Ruthwik Krishna Bandreddy <86421510+Ruthwik2610@users.noreply.github.com>
2024-09-29 19:39:27 +0530

    Update peer-chaincode-external-launcher.sh
    made it more comprehensive
    
    Signed-off-by: Ruthwik Krishna Bandreddy <86421510+Ruthwik2610@users.noreply.github.com>

--- fabric-chaincode-shim/tests/peer-chaincode-external-launcher.sh
+++ fabric-chaincode-shim/tests/peer-chaincode-external-launcher.sh
@@ -1,99 +1,81 @@
-#!/bin/bash -exu
+#!/bin/bash -eux
 
-# From https://github.com/hyperledger/fabric docs/source/peer-chaincode-devmode.md
+# Hyperledger Fabric Chaincode Deployment Script
+# Enhancements based on the latest Hyperledger Fabric release
 
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
 SRCDIR=$(realpath $(dirname $0))
-
 ORIGDIR=$PWD
-
 DESTDIR=$1
 
+LOG_DIR="$DESTDIR/logs"
+CHAINCODE_PATH="$SRCDIR/pkg"
+CHAINCODE_NAME="mycc"
+CHAINCODE_LABEL="mycc"
+CHAINCODE_TYPE="ocaml"
+CHAINCODE_METADATA_DIR="$DESTDIR/package"
+CHAINCODE_TAR_GZ="$CHAINCODE_METADATA_DIR/${CHAINCODE_NAME}.tar.gz"
+CHAINCODE_ID=""
+FABRIC_CFG_PATH="$DESTDIR/fabric/sampleconfig"
+
+# Ensure necessary directories exist
 mkdir -p $DESTDIR
+mkdir -p $LOG_DIR
 
 cd $DESTDIR
 
-# 1 -- Clone
-git clone https://github.com/hyperledger/fabric --depth=1
+# 1 -- Clone Fabric repository (latest stable)
+git clone https://github.com/hyperledger/fabric --depth 1
 cd fabric
 
-# Additional change configuration
+# 1.5 -- Modify configuration files
 sed -i sampleconfig/core.yaml sampleconfig/orderer.yaml -e "s&/var/hyperledger/production&$PWD/../tmp&g"
 sed -i sampleconfig/core.yaml -e "s&127.0.0.1:9443&127.0.0.1:9447&g"
 sed -i sampleconfig/core.yaml -e "s&0.0.0.0:7051&127.0.0.1:7051&g"
 
-# 2 -- Build
+# 2 -- Build Fabric binaries
 make orderer peer configtxgen
 
-# 2.5 create package
-rm -rf $DESTDIR/package; mkdir $DESTDIR/package; cd $DESTDIR/package
-echo '{"path":"fabric-chaincode-ocaml/tests/simple","type":"ocaml","label":"mycc"}' > metadata.json
+# 2.5 -- Create chaincode package
+rm -rf $CHAINCODE_METADATA_DIR; mkdir -p $CHAINCODE_METADATA_DIR
+echo "{\"path\":\"fabric-chaincode-ocaml/tests/simple\",\"type\":\"$CHAINCODE_TYPE\",\"label\":\"$CHAINCODE_LABEL\"}" > $CHAINCODE_METADATA_DIR/metadata.json
 
-tar -czf code.tar.gz -C $SRCDIR/pkg --exclude=_build --exclude=.git --exclude=_opam --exclude=pkg . ../../../../fabric-chaincode-ocaml/
-tar -czf mycc.tar.gz code.tar.gz metadata.json
+tar -czf code.tar.gz -C $CHAINCODE_PATH --exclude=_build --exclude=.git --exclude=_opam --exclude=pkg .
+tar -czf $CHAINCODE_TAR_GZ code.tar.gz metadata.json
 
-cd $DESTDIR/fabric
+# 2.6 -- Create external launcher scripts
+rm -rf $DESTDIR/bin/; mkdir -p $DESTDIR/bin/
 
-# 2.6 create external launcher
-rm -rf $DESTDIR/bin/; mkdir $DESTDIR/bin/
-
-cat - > $DESTDIR/bin/detect <<EOF
+# Detect script
+cat > $DESTDIR/bin/detect <<EOF
 #!/bin/sh -eux
-
-rm -f /tmp/detect.log
 exec 1>/tmp/detect.log 2>&1
-
 CHAINCODE_METADATA_DIR="\$2"
-
-pwd
-env
-tree \$1
-tree \$2
-which jq
-echo "\$CHAINCODE_METADATA_DIR/metadata.json"
-cat "\$CHAINCODE_METADATA_DIR/metadata.json"
-
-echo "\$(jq -r .type "\$CHAINCODE_METADATA_DIR/metadata.json" | tr '[:upper:]' '[:lower:]')"
-
-# use jq to extract the chaincode type from metadata.json and exit with
-# success if the chaincode type is ocaml
-if [ "\$(jq -r .type "\$CHAINCODE_METADATA_DIR/metadata.json" | tr '[:upper:]' '[:lower:]')" = "ocaml" ]; then
+if [ "\$(jq -r .type "\$CHAINCODE_METADATA_DIR/metadata.json" | tr '[:upper:]' '[:lower:]')" = "$CHAINCODE_TYPE" ]; then
     exit 0
 fi
-
 exit 1
 EOF
 
-cat - > $DESTDIR/bin/build <<EOF
+# Build script
+cat > $DESTDIR/bin/build <<EOF
 #!/bin/sh -eux
-
-rm -f /tmp/build.log
 exec 1>/tmp/build.log 2>&1
-
-date
-
 CHAINCODE_SOURCE_DIR="\$1"
-CHAINCODE_METADATA_DIR="\$2"
 BUILD_OUTPUT_DIR="\$3"
-
 dune build --root=\$CHAINCODE_SOURCE_DIR
 dune install --root=\$CHAINCODE_SOURCE_DIR --prefix=\$BUILD_OUTPUT_DIR --verbose
 echo done
 EOF
 
-cat - > $DESTDIR/bin/run <<EOF
+# Run script
+cat > $DESTDIR/bin/run <<EOF
 #!/bin/sh -eux
-
 exec 1>/tmp/run.log 2>&1
-
-date
-
 BUILD_OUTPUT_DIR="\$1"
 RUN_METADATA_DIR="\$2"
-
-# setup the environment expected by the go chaincode shim
 export CORE_CHAINCODE_ID_NAME="\$(jq -r .chaincode_id "\$RUN_METADATA_DIR/chaincode.json")"
 export CORE_PEER_TLS_ENABLED="true"
 export CORE_TLS_CLIENT_CERT_FILE="\$RUN_METADATA_DIR/client.crt"
@@ -101,52 +83,46 @@ export CORE_TLS_CLIENT_KEY_FILE="\$RUN_METADATA_DIR/client.key"
 export CORE_PEER_TLS_ROOTCERT_FILE="\$RUN_METADATA_DIR/root.crt"
 export CORE_PEER_LOCALMSPID="\$(jq -r .mspid "\$RUN_METADATA_DIR/chaincode.json")"
 PEER_ADDRESS="\$(jq -r .peer_address "\$RUN_METADATA_DIR/chaincode.json")"
-
 \$BUILD_OUTPUT_DIR/bin/chaincode \$PEER_ADDRESS \$CORE_CHAINCODE_ID_NAME
 EOF
 
 chmod u+x $DESTDIR/bin/*
 
-# 3 -- PATH
+# 3 -- Set Fabric environment variables
 export CORE_CHAINCODE_EXTERNALBUILDERS="[{name: ocaml, path: \"$DESTDIR/\"}]"
 export PATH=$(pwd)/build/bin:$PATH
-
-# 4 -- Set config path
 export FABRIC_CFG_PATH=$(pwd)/sampleconfig
 
-# 5 -- Genesis block
+# 4 -- Create Genesis block
 rm -rf sampleconfig/genesisblock ch1.block ch1.tx $DESTDIR/tmp
 configtxgen -profile SampleDevModeSolo -channelID syschannel -outputBlock genesisblock -configPath $FABRIC_CFG_PATH
 
-# 6 -- Start orderer
-ORDERER_GENERAL_GENESISPROFILE=SampleDevModeSolo orderer >& orderer.log &
+# 5 -- Start orderer
+ORDERER_GENERAL_GENESISPROFILE=SampleDevModeSolo orderer >& $LOG_DIR/orderer.log &
 
-timeout 2 tail -f orderer.log || true
+timeout 2 tail -f $LOG_DIR/orderer.log || true
 
-# 7 -- Start peer
-FABRIC_LOGGING_SPEC=chaincode=debug CORE_PEER_CHAINCODELISTENADDRESS=127.0.0.1:7052 peer node start >& peer.log &
+# 6 -- Start peer
+FABRIC_LOGGING_SPEC=chaincode=debug CORE_PEER_CHAINCODELISTENADDRESS=127.0.0.1:7052 peer node start >& $LOG_DIR/peer.log &
 
-timeout 5 tail -f peer.log || true
+timeout 5 tail -f $LOG_DIR/peer.log || true
 
-# 8 -- Create channel ch1
+# 7 -- Create channel ch1
 configtxgen -channelID ch1 -outputCreateChannelTx ch1.tx -profile SampleSingleMSPChannel -configPath $FABRIC_CFG_PATH
 peer channel create -o 127.0.0.1:7050 -c ch1 -f ch1.tx
-
 peer channel join -b ch1.block
 
-# 9 Install custom chaincode
-peer lifecycle chaincode install $DESTDIR/package/mycc.tar.gz |& tee chaincode-install.log
+# 8 -- Install chaincode
+peer lifecycle chaincode install $CHAINCODE_TAR_GZ |& tee $LOG_DIR/chaincode-install.log
+CHAINCODE_ID=$(sed -ne "s/^.*code package identifier: $CHAINCODE_NAME:\([a-z0-9]*\).*$/\1/p" $LOG_DIR/chaincode-install.log)
+test -n "$CHAINCODE_ID"
 
-ID=$(sed -ne "s/^.*code package identifier: mycc:\([a-z0-9]*\).*$/\1/p" chaincode-install.log)
+# 9 -- Approve and commit the chaincode definition
+peer lifecycle chaincode approveformyorg -o 127.0.0.1:7050 --channelID ch1 --name $CHAINCODE_NAME --version 1.0 --sequence 1 --init-required --signature-policy "OR ('SampleOrg.member')" --package-id $CHAINCODE_NAME:$CHAINCODE_ID
+peer lifecycle chaincode checkcommitreadiness -o 127.0.0.1:7050 --channelID ch1 --name $CHAINCODE_NAME --version 1.0 --sequence 1 --init-required --signature-policy "OR ('SampleOrg.member')"
+peer lifecycle chaincode commit -o 127.0.0.1:7050 --channelID ch1 --name $CHAINCODE_NAME --version 1.0 --sequence 1 --init-required --signature-policy "OR ('SampleOrg.member')" --peerAddresses 127.0.0.1:7051
 
-test -n "$ID"
-
-# 11 -- Approve and commit the chaincode definition
-peer lifecycle chaincode approveformyorg -o 127.0.0.1:7050 --channelID ch1 --name mycc --version 1.0 --sequence 1 --init-required --signature-policy "OR ('SampleOrg.member')" --package-id mycc:$ID
-peer lifecycle chaincode checkcommitreadiness -o 127.0.0.1:7050 --channelID ch1 --name mycc --version 1.0 --sequence 1 --init-required --signature-policy "OR ('SampleOrg.member')"
-peer lifecycle chaincode commit -o 127.0.0.1:7050 --channelID ch1 --name mycc --version 1.0 --sequence 1 --init-required --signature-policy "OR ('SampleOrg.member')" --peerAddresses 127.0.0.1:7051
-
-# Invoke chaincode
-CORE_PEER_ADDRESS=127.0.0.1:7051 peer chaincode invoke -o 127.0.0.1:7050 -C ch1 -n mycc -c '{"Args":["init","a","100","b","200"]}' --isInit
+# 10 -- Invoke chaincode
+CORE_PEER_ADDRESS=127.0.0.1:7051 peer chaincode invoke -o 127.0.0.1:7050 -C ch1 -n $CHAINCODE_NAME -c '{"Args":["init","a","100","b","200"]}' --isInit
 
 cd $ORIGDIR
